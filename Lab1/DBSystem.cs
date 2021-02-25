1629585080c140ecee1c9e30ffe2ca536c46274c
VsIG <Valentyn2202@gmail.com>
2021-02-25 17:12:05 +0200

    ✨: add [DBSystem.cs]

---
+++ Lab1/DBSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+	class DBSystem
+	{
+		public Reviews GetReviews(int reviewId)
+		{
+			return new Reviews();
+		}
+
+		public Hotels GetRoom(int id)
+		{
+			return new Hotels();
+		}
+
+		public User GetUser(int id)
+		{
+			return new User();
+		}
+
+		public Hotels GetHotel(int id)
+		{
+			return Hotels();
+		}
+	}
+}
