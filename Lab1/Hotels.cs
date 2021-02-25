ee2629dd41cf3c37da404105614f8b4569d54981
VsIG <Valentyn2202@gmail.com>
2021-02-25 16:05:27 +0200

    ✨: add constructor to [Hotels]

--- Lab1/Hotels.cs
+++ Lab1/Hotels.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Hotel
+﻿namespace Hotel
 {
-    class Hotels
-    {
-        int CountryId;
-        int OwnerId;
-        int NumberOfStarsInt;
-        string DescriptionStr;
-        int LocationInt;
-        int HotelTypeInt;
-        int RatingInt;
-        string HotelNameStr;
-    }
+	class Hotels
+	{
+		int CountryId;
+		int OwnerId;
+		int NumberOfStarsInt;
+		string DescriptionStr;
+		int LocationInt;
+		int HotelTypeInt;
+		int RatingInt;
+		string HotelNameStr;
+
+		public Hotels(int countryID, int ownerID, int NumOfStars,
+			string description, int location, int hotelType,
+			int rating, string hotelName)
+		{
+			CountryId = countryID;
+			OwnerId = ownerID;
+			NumberOfStarsInt = NumOfStars;
+			DescriptionStr = description;
+			LocationInt = location;
+			HotelTypeInt = hotelType;
+			RatingInt = rating;
+			HotelNameStr = hotelName;
+		}
+	}
 }
