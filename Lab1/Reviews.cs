b5c01e1e22801615b98c4186b8aa8d12596f629f
VsIG <Valentyn2202@gmail.com>
2021-02-25 16:32:25 +0200

    ✨: add constructor and properties to [Reviews.cs]

--- Lab1/Reviews.cs
+++ Lab1/Reviews.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Hotel
 {
-    class Reviews
-    {
-        DateTime CreationDate;
-        int HotelId;
-        int RatingInt;
-        int ReviewId;
-        string ReviewStr;
-        int UserId;
-    }
+	class Reviews
+	{
+		DateTime CreationDate { get; set; }
+		int HotelId { get; set; }
+		int RatingInt { get; set; }
+		int ReviewId { get; set; }
+		string ReviewStr { get; set; }
+		int UserId { get; set; }
+
+		public Reviews(DateTime date, int hotelID,
+			int rating, int reviewID, string reviewStr,
+			int user)
+		{
+			CreationDate = date;
+			HotelId = hotelID;
+			RatingInt = rating;
+			ReviewId = reviewID;
+			ReviewStr = reviewStr;
+			UserId = user;
+		}
+	}
 }
