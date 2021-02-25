0c3860c799506933218c393d9f16de66ee5ff87c
VsIG <Valentyn2202@gmail.com>
2021-02-25 16:06:42 +0200

    ✨: delete 'public statements'

--- Lab1/Rooms.cs
+++ Lab1/Rooms.cs
@@ -6,15 +6,15 @@ namespace Hotel
 {
     class Rooms
     {
-        public int HotelId { get; set; }
-        public int PriceInt { get; set; }
-        public int RoomNumberInt { get; set; }
-        public int FreeDatesInt { get; set; }
-        public bool TVBool { get; set; }
-        public string RoomTypeStr { get; set; }
-        public int NumberOfBedsInt { get; set; }
-        public bool BalconyBool { get; set; }
-        public bool SaleBool { get; set; }
+        int HotelId { get; set; }
+        int PriceInt { get; set; }
+        int RoomNumberInt { get; set; }
+        int FreeDatesInt { get; set; }
+        bool TVBool { get; set; }
+        string RoomTypeStr { get; set; }
+        int NumberOfBedsInt { get; set; }
+        bool BalconyBool { get; set; }
+        bool SaleBool { get; set; }
 
         public Rooms(int id, int price, int roomNum, int freeDates,
             bool tvBool, string roomType, int bedsNum, bool balconyBool,
