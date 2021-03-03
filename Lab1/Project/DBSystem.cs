3b3b10e9c6f77d816d33ff501de538480c85bdf5
VsIG <valentyn2202@gmail.com>
2021-03-03 19:53:50 +0200

    ✨: add enter class

--- Lab1/Project/DBSystem.cs
+++ Lab1/Project/DBSystem.cs
@@ -4,17 +4,11 @@ using System.Text;
 
 namespace Hotel
 {
-	class DBSystem
+	public class DBSystem
 	{
 		public DBSystem()
 		{
-			User Admin = new User(1, 1, "FirstUser",
-			"RandomEmail@gmail.com", "88005553535");
-			Role AdminRole = new Role(1, "Admin");
-			Admin.Roles.Add(AdminRole);
-			Hotel FirstHotel = new Hotel(1, "First", "KPI Street");
-			Admin.AddHotel();
-			Console.WriteLine("DBSystem has started");
+
 		}
 	}
 }
