e9c83e86c48e7ce46451421e85979c63c63988a9
VsIG <valentyn2202@gmail.com>
2021-03-03 18:49:05 +0200

    📝: reorganise files

--- Lab1/Project/DBSystem.cs
+++ Lab1/Project/DBSystem.cs
@@ -6,24 +6,5 @@ namespace Hotel
 {
 	class DBSystem
 	{
-		public Reviews GetReviews(int reviewId)
-		{
-			return new Reviews();
-		}
-
-		public Hotels GetRoom(int id)
-		{
-			return new Hotels();
-		}
-
-		public User GetUser(int id)
-		{
-			return new User();
-		}
-
-		public Hotels GetHotel(int id)
-		{
-			return new Hotels();
-		}
 	}
 }
