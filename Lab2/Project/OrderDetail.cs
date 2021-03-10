21a11c79db0c5ac4da9730e6898f395268f1d658
VsIG <valentyn2202@gmail.com>
2021-03-10 17:23:11 +0200

    ✨: add Lab2 with constructors

---
+++ Lab2/Project/OrderDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+	public class OrderDetail
+	{
+		public int OrderDetailID { get; set; }
+		public Order Order { get; set; }
+		public Room Room { get; set; }
+		public DateTime DateStart { get; set; }
+		public DateTime DateEnd { get; set; }
+		public decimal Price { get; set; }
+		public string Comment { get; set; }
+
+		public OrderDetail(int OrderDetailID, int OrderID, DateTime DateStart, DateTime DateEnd, decimal Price, string Comment)
+		{
+			this.OrderDetailID = OrderDetailID;
+			this.DateStart = DateStart;
+			this.DateEnd = DateEnd;
+			this.Price = Price;
+			this.Comment = Comment;
+		}
+	}
+}
---
+++ Lab2/Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Hotel
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("First Lab has started");
+            User AdminV = new User(1, 2, "AdminValera", "valera69@gmail.com",
+                "+0690696969");
+            Hotel Hotel1 = new Hotel(1, "Адам", "вул. Т. Шевченка");
+            Hotel1.City = City.Kyiv;
+            Console.WriteLine("First Team: Zavalniuk Maxim,\nDmytrenko Roman,\n" +
+                "Sichkar Tetiana,\nDominskyi Valentyn");
+            Console.ReadLine();
+        }
+    }
+}
