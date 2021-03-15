4a123895054e8da6cea03522cf14c90174a9b03f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-15 20:57:45 +0200

    Update User.cs
    Set validator for mail

--- Lab3/Project/User.cs
+++ Lab3/Project/User.cs
@@ -1,75 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Hotel
 {
-	public class User
-	{
-		public string Login { get; set; }
-		public string Password { get; set; }
-		public int UserID { get; set; }
-		public short UserType { get; set; }
-		public string UserName { get; set; }
-		public string UserMail { get; set; }
-		public string UserPhone { get; set; }
-		public List<Order> Orders = new List<Order>();
+    public class User
+    {
+        private string usermail;
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int UserID { get; set; }
+        public short UserType { get; set; }
+        public string UserName { get; set; }
+        public string UserMail
+        {
+            get
+            {
+                return usermail;   
+            }
+            set
+            {
+                try
+                {
+                    var address = new System.Net.Mail.MailAddress(value);
+                    usermail = address.Address;
+                }
+                catch
+                {
+                    Console.WriteLine("Введіть дійсну електронну пошту");
+                }
+            }
+        }
+        public string UserPhone { get; set; }
+        public List<Order> Orders = new List<Order>();
 
-		/// <summary>
-		/// Constructor by default
-		/// </summary>
-		public User()
-		{
-			Console.WriteLine("Default User is created");
-		}
+        /// <summary>
+        /// Constructor by default
+        /// </summary>
+        public User()
+        {
+            Console.WriteLine("Default User is created");
+        }
 
-		/// <summary>
-		/// Copy constructor
-		/// </summary>
-		/// <param name="tech"></param>
-		public User(User user)
-		{
-			UserID = user.UserID;
-			UserType = user.UserType;
-			UserName = user.UserName;
-			UserMail = user.UserMail;
-			UserPhone = user.UserPhone;
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        /// <param name="tech"></param>
+        public User(User user)
+        {
+            UserID = user.UserID;
+            UserType = user.UserType;
+            UserName = user.UserName;
+            UserMail = user.UserMail;
+            UserPhone = user.UserPhone;
 
-			Console.WriteLine("Copy User is created");
-		}
+            Console.WriteLine("Copy User is created");
+        }
 
-		/// <summary>
-		/// Initialization Constructor
-		/// </summary>
-		/// <param name="UserID"></param>
-		/// <param name="UserType"></param>
-		/// <param name="UserName"></param>
-		/// <param name="UserMail"></param>
-		/// <param name="UserPhone"></param>
-		public User(int UserID, short UserType,
-		            string UserName, string UserMail, string UserPhone)
-		{
-			this.UserID = UserID;
-			this.UserType = UserType;
-			this.UserName = UserName;
-			this.UserMail = UserMail;
-			this.UserPhone = UserPhone;
+        /// <summary>
+        /// Initialization Constructor
+        /// </summary>
+        /// <param name="UserID"></param>
+        /// <param name="UserType"></param>
+        /// <param name="UserName"></param>
+        /// <param name="UserMail"></param>
+        /// <param name="UserPhone"></param>
+        public User(int UserID, short UserType,
+                    string UserName, string UserMail, string UserPhone)
+        {
+            this.UserID = UserID;
+            this.UserType = UserType;
+            this.UserName = UserName;
+            this.UserMail = UserMail;
+            this.UserPhone = UserPhone;
 
-			Console.WriteLine("User is created");
-		}
+            Console.WriteLine("User is created");
+        }
 
-		public void SearchForInfo() { }
-		public void Book() { }
-		public void CheckBooking() { }
-		public void CancelBooking() { }
-		public void LogOut() { }
-		public void AddHotel() { }
-		public void AddCity(string city) { }
-		private void SingUpLogin() { }
-		public void UpdateHotelInfo() { }
-		public void CheckHotelBooking() { }
-		public void UpdateRoom() { }
-		public void ChangeAccess() { }
-		public void RemoveHotel() { }
-	}
+        public void SearchForInfo() { }
+        public void Book() { }
+        public void CheckBooking() { }
+        public void CancelBooking() { }
+        public void LogOut() { }
+        public void AddHotel() { }
+        public void AddCity(string city) { }
+        private void SingUpLogin() { }
+        public void UpdateHotelInfo() { }
+        public void CheckHotelBooking() { }
+        public void UpdateRoom() { }
+        public void ChangeAccess() { }
+        public void RemoveHotel() { }
+    }
 }
