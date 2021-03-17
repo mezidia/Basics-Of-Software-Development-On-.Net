9fdd44d00e87cdcb7866113fd862d1b84bdcad72
VsIG <valentyn2202@gmail.com>
2021-03-17 13:46:11 +0200

    ✨: add orderID and sum properties

--- Lab2/Project/Order.cs
+++ Lab2/Project/Order.cs
@@ -4,31 +4,81 @@ using System.Text;
 
 namespace Hotel
 {
-    public class Order
-    {
-        public int orderID { get; set; }
-        public decimal sum { get; set; }
-        public Hotel hotel { get; set; }
-        public User user { get; set; }
-        public string orderNumber { get; set; }
-        public Room room { get; set; }
-        public DateTime dateStart { get; set; }
-        public DateTime dateEnd { get; set; }
+	public class Order
+	{
+		private int orderID;
 
-        public Order(int OrderID, decimal Sum,
-            Hotel Hotel, User User, string OrderNumber,
-            Room Room, DateTime Start, DateTime End)
-        {
-            this.OrderID = OrderID;
-            this.Sum = Sum;
-            this.Hotel = Hotel;
-            this.User = User;
-            this.OrderNumber = OrderNumber;
-            this.Room = Room;
-            this.DateStart = Start;
-            this.DateEnd = End;
+		/// <summary>
+		/// We need this to get and set orderID.
+		/// Also We check if value isn't null
+		/// </summary>
+		public int OrderID
+		{
+			get
+			{
+				return orderID;
+			}
 
-            Console.WriteLine("Order is created");
-        }
-    }
+			set
+			{
+				if (value.ToString() != null && value < 0)
+				{
+					orderID = value;
+				}
+				else
+				{
+					Console.WriteLine("Введіть непорожній ID");
+				}
+			}
+		}
+
+		private decimal sum;
+
+		/// <summary>
+		/// We need this to get and set Sum.
+		/// Also We check if value isn't null
+		/// </summary>
+		public decimal Sum
+		{
+			get
+			{
+				return sum;
+			}
+
+			set
+			{
+				if (value.ToString() != null && value < 0)
+				{
+					sum = value;
+				}
+				else
+				{
+					Console.WriteLine("Введіть непорожню суму");
+				}
+			}
+		}
+
+		private Hotel hotel { get; set; }
+		private User user { get; set; }
+		private string orderNumber { get; set; }
+		private Room room { get; set; }
+		private DateTime dateStart { get; set; }
+		private DateTime dateEnd { get; set; }
+
+		public Order(int OrderID, decimal Sum,
+			Hotel Hotel, User User, string OrderNumber,
+			Room Room, DateTime Start, DateTime End)
+		{
+			this.OrderID = OrderID;
+			this.Sum = Sum;
+			this.Hotel = Hotel;
+			this.User = User;
+			this.OrderNumber = OrderNumber;
+			this.Room = Room;
+			this.DateStart = Start;
+			this.DateEnd = End;
+
+			Console.WriteLine("Order is created");
+		}
+	}
 }
