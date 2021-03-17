b99fd989a4b55a94adc443af7604a2594fff3898
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2021-03-17 11:51:13 +0200

    Update Hotel.cs

--- Lab3/Project/Hotel.cs
+++ Lab3/Project/Hotel.cs
@@ -6,13 +6,86 @@ namespace Hotel
 {
 	public class Hotel
 	{
-		public int NumberOfStars;
-		public string Description;
-		public int RatingInt;
-		public int HotelID { get; set; }
-		public string HotelName { get; set; }
+		private int numberofstars;
+        private string description;
+		private int ratingint;
+		private int hotelid;
+		private string hotelname;
+		private string adress;
+
+		public int NumberOfStars
+        {
+			get
+			{
+				return numberofstars;
+			}
+			set
+			{
+				if (value.ToString() != null && value >= 1 && value <= 5) numberofstars = value;
+				else Console.WriteLine("Введіть правильну кількість зірок.");
+			}
+        }
+		public string Description
+        {
+			get
+			{
+				return description;
+			}
+			set
+			{
+				if (value.Length() > 20) description = value;
+				else Console.WriteLine("Введіть більшу кількість символів");
+			}
+        }
+		public int RatingInt
+        {
+			get
+			{
+				return ratingint;
+			}
+			set
+            {
+				if (value.ToString() != null) ratingint = value;
+				else Console.WriteLine("Введіть не пустий ідентифікатор рейтингу");
+            }
+        }
+		public int HotelID
+		{
+			get
+			{
+				return hotelid;
+			}
+			set
+			{
+				if (value.ToString() != null) hotelid = value;
+				else Console.WriteLine("Введіть не пустий ідентифікатор готелю.");
+			}
+		}
+		public string HotelName 
+		{ 
+			get
+            {
+				return hotelname;
+            }
+			set
+            {
+				if (value.Length() < 15) hotelname = value;
+				else Console.WriteLine("Забагато символів для імені готелю.");
+            }
+		}
 		public City City { get; set; }
-		public string Adress { get; set; }
+		public string Adress 
+		{
+			get 
+			{
+				return adress;
+			}
+			set
+            {
+				if (value.Length() < 1) adress = value;
+				else Console.WriteLine("Невірна адреса");
+            }
+		}
 		public List<Room> Rooms = new List<Room>();
 		public List<Order> Orders = new List<Order>();
 
