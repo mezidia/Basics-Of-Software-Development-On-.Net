2bb0e320e76dfd857aba55c8d2cad06981b68b13
VsIG <valentyn2202@gmail.com>
2021-03-17 14:03:28 +0200

    ✨: Missed the order of labs

--- Lab2/Project/Order.cs
+++ Lab2/Project/Order.cs
@@ -1,231 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Hotel
 {
-	public class Order
-	{
-		private int orderID;
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public decimal Sum { get; set; }
+        public Hotel Hotel { get; set; }
+        public User User { get; set; }
+        public string OrderNumber { get; set; }
+        public Room Room { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
 
-		/// <summary>
-		/// We need this to get and set orderID.
-		/// Also We check if value isn't null
-		/// </summary>
-		public int OrderID
-		{
-			get
-			{
-				return orderID;
-			}
+        public Order(int OrderID, decimal Sum,
+            Hotel Hotel, User User, string OrderNumber,
+            Room Room, DateTime Start, DateTime End)
+        {
+            this.OrderID = OrderID;
+            this.Sum = Sum;
+            this.Hotel = Hotel;
+            this.User = User;
+            this.OrderNumber = OrderNumber;
+            this.Room = Room;
+            this.DateStart = Start;
+            this.DateEnd = End;
 
-			set
-			{
-				if (value.ToString() != null && value < 0)
-				{
-					orderID = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожній ID");
-				}
-			}
-		}
-
-		private decimal sum;
-
-		/// <summary>
-		/// We need this to get and set Sum.
-		/// Also We check if value isn't null
-		/// </summary>
-		public decimal Sum
-		{
-			get
-			{
-				return sum;
-			}
-
-			set
-			{
-				if (value.ToString() != null && value < 0)
-				{
-					sum = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожню суму");
-				}
-			}
-		}
-
-		private Hotel hotel;
-
-		/// <summary>
-		/// We need this to get and set Hotel.
-		/// Also We check if value isn't null
-		/// </summary>
-		public Hotel Hotel
-		{
-			get
-			{
-				return hotel;
-			}
-
-			set
-			{
-				if (value != null)
-				{
-					hotel = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожній готель");
-				}
-			}
-		}
-
-		private User user;
-
-		/// <summary>
-		/// We need this to get and set User.
-		/// Also We check if value isn't null
-		/// </summary>
-		public User User
-		{
-			get
-			{
-				return user;
-			}
-
-			set
-			{
-				if (value != null)
-				{
-					user = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожнього користувача");
-				}
-			}
-		}
-
-		private string orderNumber;
-
-		/// <summary>
-		/// We need this to get and set OrderNumber.
-		/// Also We check if value isn't null
-		/// </summary>
-		public string OrderNumber
-		{
-			get
-			{
-				return orderNumber;
-			}
-
-			set
-			{
-				if (value != null)
-				{
-					orderNumber = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожній номер замовлення");
-				}
-			}
-		}
-
-		private Room room;
-
-		/// <summary>
-		/// We need this to get and set Order.
-		/// Also We check if value isn't null
-		/// </summary>
-		public Room Room
-		{
-			get
-			{
-				return room;
-			}
-
-			set
-			{
-				if (value != null)
-				{
-					room = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожню кімнату");
-				}
-			}
-		}
-
-		private DateTime dateStart;
-
-		/// <summary>
-		/// We need this to get and set Order.
-		/// Also We check if value isn't null
-		/// </summary>
-		public DateTime DateStart
-		{
-			get
-			{
-				return dateStart;
-			}
-
-			set
-			{
-				if (value != null)
-				{
-					dateStart = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожню дату початку");
-				}
-			}
-		}
-
-		private DateTime dateEnd;
-
-		/// <summary>
-		/// We need this to get and set Order.
-		/// Also We check if value isn't null
-		/// </summary>
-		public DateTime DateEnd
-		{
-			get
-			{
-				return dateEnd;
-			}
-
-			set
-			{
-				if (value != null)
-				{
-					dateEnd = value;
-				}
-				else
-				{
-					Console.WriteLine("Введіть непорожню дату закінчення");
-				}
-			}
-		}
-
-		public Order(int orderID, decimal sum,
-			Hotel hotel, User user, string orderNumber,
-			Room room, DateTime start, DateTime end)
-		{
-			OrderID = orderID;
-			Sum = sum;
-			Hotel = hotel;
-			User = user;
-			OrderNumber = orderNumber;
-			Room = room;
-			DateStart = start;
-			DateEnd = end;
-
-			Console.WriteLine("Order is created");
-		}
-	}
+            Console.WriteLine("Order is created");
+        }
+    }
 }
