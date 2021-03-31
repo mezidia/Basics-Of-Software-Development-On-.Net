8ff7e6bc8ab08c5db73648e698658306634ee149
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-03-31 14:54:24 +0300

    Update Room.cs

--- Lab4/Project/Room.cs
+++ Lab4/Project/Room.cs
@@ -2,282 +2,314 @@
 
 namespace Hotel
 {
-	public class Room
-	{
-		private int roomID;
+    public class Room
+    {
+        private int roomID;
 
-		/// <summary>
-		/// get and set roomID
-		/// check if value is positive
-		/// </summary>
-		public int RoomID
-		{
-			get
-			{
-				return roomID;
-			}
-			set
-			{
-				if (value > 0)
-				{
-					roomID = value;
-				}
-				else
-				{
-					Console.WriteLine("ID має бути бiльшим вiд 0");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set roomID
+        /// check if value is positive
+        /// </summary>
+        public int RoomID
+        {
+            get
+            {
+                return roomID;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    roomID = value;
+                }
+                else
+                {
+                    Console.WriteLine("ID має бути бiльшим вiд 0");
+                }
+            }
+        }
 
-		private int roomNumber;
+        private int roomNumber;
 
-		/// <summary>
-		/// get and set roomNumber
-		/// check if value is positive
-		/// </summary>
-		public int RoomNumber
-		{
-			get
-			{
-				return roomNumber;
-			}
-			set
-			{
-				if (value > 0)
-				{
-					roomNumber = value;
-				}
-				else
-				{
-					Console.WriteLine("Номер кiмнати має бути бiльшим від 0");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set roomNumber
+        /// check if value is positive
+        /// </summary>
+        public int RoomNumber
+        {
+            get
+            {
+                return roomNumber;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    roomNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Номер кiмнати має бути бiльшим від 0");
+                }
+            }
+        }
 
-		private string roomName;
+        private string roomName;
 
-		/// <summary>
-		/// get and set roomName
-		/// check if value is not empty
-		/// </summary>
-		public string RoomName
-		{
-			get
-			{
-				return roomName;
-			}
-			set
-			{
-				if (!string.IsNullOrEmpty(value) &&
-					!string.IsNullOrWhiteSpace(value))
-				{
-					roomName = value;
-				}
-				else
-				{
-					Console.WriteLine("Назва кiмнати не повинна бути пустою");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set roomName
+        /// check if value is not empty
+        /// </summary>
+        public string RoomName
+        {
+            get
+            {
+                return roomName;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) &&
+                    !string.IsNullOrWhiteSpace(value))
+                {
+                    roomName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Назва кiмнати не повинна бути пустою");
+                }
+            }
+        }
 
-		private int roomSize;
+        private int roomSize;
 
-		/// <summary>
-		/// get and set roomSize
-		/// check if value is positive
-		/// </summary>
-		public int RoomSize
-		{
-			get
-			{
-				return roomSize;
-			}
-			set
-			{
-				if (value > 0)
-				{
-					roomSize = value;
-				}
-				else
-				{
-					Console.WriteLine("Розмiр кiмнати має бути бiльшим вiд 0");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set roomSize
+        /// check if value is positive
+        /// </summary>
+        public int RoomSize
+        {
+            get
+            {
+                return roomSize;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    roomSize = value;
+                }
+                else
+                {
+                    Console.WriteLine("Розмiр кiмнати має бути бiльшим вiд 0");
+                }
+            }
+        }
 
-		private Hotel hotel;
+        private Hotel hotel;
 
-		/// <summary>
-		/// get and set Hotel
-		/// check if value is not empty
-		/// </summary>
-		public Hotel Hotel
-		{
-			get
-			{
-				return hotel;
-			}
-			set
-			{
-				if (value != null)
-				{
-					hotel = value;
-				}
-				else
-				{
-					Console.WriteLine("Кiмната має належати до готелю");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set Hotel
+        /// check if value is not empty
+        /// </summary>
+        public Hotel Hotel
+        {
+            get
+            {
+                return hotel;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    hotel = value;
+                }
+                else
+                {
+                    Console.WriteLine("Кiмната має належати до готелю");
+                }
+            }
+        }
 
-		private bool? tv;
+        private bool? tv;
 
-		/// <summary>
-		/// get and set TV
-		/// check if value is not empty
-		/// </summary>
-		public bool? TV
-		{
-			get
-			{
-				return tv;
-			}
-			set
-			{
-				if (value.HasValue)
-				{
-					tv = value;
-				}
-				else
-				{
-					Console.WriteLine("Введiть значення про наявнiсть телевiзора");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set TV
+        /// check if value is not empty
+        /// </summary>
+        public bool? TV
+        {
+            get
+            {
+                return tv;
+            }
+            set
+            {
+                if (value.HasValue)
+                {
+                    tv = value;
+                }
+                else
+                {
+                    Console.WriteLine("Введiть значення про наявнiсть телевiзора");
+                }
+            }
+        }
 
-		private string roomType;
+        private string roomType;
 
-		/// <summary>
-		/// get and set roomType
-		/// check if value is not empty
-		/// </summary>
-		public string RoomType
-		{
-			get
-			{
-				return roomType;
-			}
-			set
-			{
-				if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
-				{
-					roomType = value;
-				}
-				else
-				{
-					Console.WriteLine("Задайте тип кiмнати");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set roomType
+        /// check if value is not empty
+        /// </summary>
+        public string RoomType
+        {
+            get
+            {
+                return roomType;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+                {
+                    roomType = value;
+                }
+                else
+                {
+                    Console.WriteLine("Задайте тип кiмнати");
+                }
+            }
+        }
 
-		private int numberOfBeds;
+        private int numberOfBeds;
 
-		/// <summary>
-		/// get and set numberOfBeds
-		/// check if value is positive
-		/// </summary>
-		public int NumberOfBeds
-		{
-			get
-			{
-				return numberOfBeds;
-			}
-			set
-			{
-				if (value > 0)
-				{
-					numberOfBeds = value;
-				}
-				else
-				{
-					Console.WriteLine("Кiлькість спальних мiсць " +
-						"має бути бiльша вiд 0");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set numberOfBeds
+        /// check if value is positive
+        /// </summary>
+        public int NumberOfBeds
+        {
+            get
+            {
+                return numberOfBeds;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    numberOfBeds = value;
+                }
+                else
+                {
+                    Console.WriteLine("Кiлькість спальних мiсць " +
+                        "має бути бiльша вiд 0");
+                }
+            }
+        }
 
-		private bool? balcony;
+        private bool? balcony;
 
-		/// <summary>
-		/// get and set balcony
-		/// check if value is not empty
-		/// </summary>
-		public bool? Balcony
-		{
-			get
-			{
-				return balcony;
-			}
-			set
-			{
-				if (value.HasValue)
-				{
-					balcony = value;
-				}
-				else
-				{
-					Console.WriteLine("Введiть значення про наявнiсть балкону");
-				}
-			}
-		}
+        /// <summary>
+        /// get and set balcony
+        /// check if value is not empty
+        /// </summary>
+        public bool? Balcony
+        {
+            get
+            {
+                return balcony;
+            }
+            set
+            {
+                if (value.HasValue)
+                {
+                    balcony = value;
+                }
+                else
+                {
+                    Console.WriteLine("Введiть значення про наявнiсть балкону");
+                }
+            }
+        }
 
-		/// <summary>
-		/// Initialization Constructor
-		/// </summary>
-		/// <param name="roomID"></param>
-		/// <param name="roomNumber"></param>
-		/// <param name="roomName"></param>
-		/// <param name="roomSize"></param>
-		/// <param name="hotel"></param>
-		/// <param name="tv"></param>
-		/// <param name="roomType"></param>
-		/// <param name="numberOfBeds"></param>
-		/// <param name="balcony"></param>
-		public Room(int roomID, int roomNumber, string roomName,
-			int roomSize, Hotel hotel, bool? tv, string roomType,
-			int numberOfBeds, bool? balcony)
-		{
-			RoomID = roomID;
-			RoomNumber = roomNumber;
-			RoomName = roomName;
-			RoomSize = roomSize;
-			Hotel = hotel;
-			TV = tv;
-			RoomType = roomType;
-			NumberOfBeds = numberOfBeds;
-			Balcony = balcony;
+        // Перевантажуємо логічний оператор &
+        public static bool operator &(Room room1, Room room2)
+        {
+            if (room1.TV == true && room2.TV == true)
+                return true;
+            return false;
+        }
 
-			Console.WriteLine($"Додано кiмнату номер {RoomNumber} готелю " +
-				$"{Hotel.HotelName}");
-		}
+        // Перевантажуємо логічний оператор !
+        public static bool operator !(Room room)
+        {
+            if (room.balcony == true)
+                return true;
+            return false;
+        }
 
-		/// <summary>
-		/// Copy constructor
-		/// </summary>
-		/// <param name="room"></param>
-		public Room(Room room)
-		{
-			RoomID = room.RoomID;
-			RoomNumber = room.RoomNumber;
-			RoomName = room.RoomName;
-			RoomSize = room.RoomSize;
-			Hotel = room.Hotel;
-			TV = room.TV;
-			RoomType = room.RoomType;
-			NumberOfBeds = room.NumberOfBeds;
-			Balcony = room.Balcony;
+        // Перевантажуємо оператор true
+        public static bool operator true(Room room)
+        {
+            if (room.TV == true)
+                return true;
+            return false;
+        }
 
-			Console.WriteLine("Додано копiю кiмнати");
-		}
-	}
+        // Перевантажуємо оператор false
+        public static bool operator false(Room room)
+        {
+            if (room.TV == false)
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Initialization Constructor
+        /// </summary>
+        /// <param name="roomID"></param>
+        /// <param name="roomNumber"></param>
+        /// <param name="roomName"></param>
+        /// <param name="roomSize"></param>
+        /// <param name="hotel"></param>
+        /// <param name="tv"></param>
+        /// <param name="roomType"></param>
+        /// <param name="numberOfBeds"></param>
+        /// <param name="balcony"></param>
+        public Room(int roomID, int roomNumber, string roomName,
+            int roomSize, Hotel hotel, bool? tv, string roomType,
+            int numberOfBeds, bool? balcony)
+        {
+            RoomID = roomID;
+            RoomNumber = roomNumber;
+            RoomName = roomName;
+            RoomSize = roomSize;
+            Hotel = hotel;
+            TV = tv;
+            RoomType = roomType;
+            NumberOfBeds = numberOfBeds;
+            Balcony = balcony;
+
+            Console.WriteLine($"Додано кiмнату номер {RoomNumber} готелю " +
+                $"{Hotel.HotelName}");
+        }
+
+        /// <summary>
+        /// Copy constructor
+        /// </summary>
+        /// <param name="room"></param>
+        public Room(Room room)
+        {
+            RoomID = room.RoomID;
+            RoomNumber = room.RoomNumber;
+            RoomName = room.RoomName;
+            RoomSize = room.RoomSize;
+            Hotel = room.Hotel;
+            TV = room.TV;
+            RoomType = room.RoomType;
+            NumberOfBeds = room.NumberOfBeds;
+            Balcony = room.Balcony;
+
+            Console.WriteLine("Додано копiю кiмнати");
+        }
+    }
 }
