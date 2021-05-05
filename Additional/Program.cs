a772d81a1315b46dfd97188b234c2263846deeb1
VsIG-official <valentyn2202@gmail.com>
2021-05-05 19:41:37 +0300

    ✨: add arrays and some try/catches

--- Additional/Program.cs
+++ Additional/Program.cs
@@ -6,7 +6,36 @@ namespace Additional
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] firstArray = { 0, 1, 2, 3 };
+			int[] secondArray = { 4, 5, 6 };
+
+			Console.WriteLine("Намагаємося отримати доступ до 5-ого" +
+				" елементу першого масиву");
+
+			try
+			{
+				Console.WriteLine($"5-ий елемент масиву = " + firstArray[4]);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Catch some exception");
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.GetType());
+			}
+
+			Console.WriteLine("Намагаємося отримати доступ до 4-ого" +
+				" елементу другого масиву");
+
+			try
+			{
+				Console.WriteLine($"4-ий елемент масиву = " + firstArray[3]);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Catch some exception");
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.GetType());
+			}
 		}
 	}
 }
