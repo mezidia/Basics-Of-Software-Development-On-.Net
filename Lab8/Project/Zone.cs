fa92bc752e30ee3aeea317019302a88949c873ae
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-05-19 15:11:36 +0300

    Create Zone.cs

---
+++ Lab8/Project/Zone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Project
+{
+    class Zone
+    {
+		private string FilePath;
+		public Zone(string filePath)
+		{
+			FilePath = filePath;
+		}
+	}
+}
