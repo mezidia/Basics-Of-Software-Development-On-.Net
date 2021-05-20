22f0e667dd0968bb6cc76e78da070ac247ef5e23
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-05-20 10:17:10 +0300

    ✨: smol rework

--- Additional/Zone43.cs
+++ Additional/Zone43.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.IO;
 using System.Threading;
 using System.Linq;
@@ -9,23 +7,26 @@ namespace Additional
 {
     public class Zone43
     {
-        #region Lab8-2
+        #region Lab8-3
 
-        private FileInfo file;
-        private Mutex fileLock = new Mutex();
+        private readonly FileInfo file;
+        private readonly Mutex fileLock = new Mutex();
 
         public Zone43(FileInfo f)
         {
             file = f;
+
             if (File.Exists(file.FullName))
             {
                 File.Delete(file.FullName);
             }
         }
+
         public string ReadFromFile(int line)
         {
             fileLock.WaitOne();
             string lastLine;
+
             using (var sr = new StreamReader(file.FullName))
             {
                 for (int i = 1; i < line; i++)
@@ -35,21 +36,24 @@ namespace Additional
                 lastLine = sr.ReadLine();
             }
             fileLock.ReleaseMutex();
+
             return lastLine;
         }
 
         public int WriteToFile(string text)
         {
             fileLock.WaitOne();
+
             using (StreamWriter w = File.AppendText(file.FullName))
                 {
                     w.WriteLine(text);
                     Console.WriteLine("Записано строку \"" + text + "\"");
-                }              
+                }
             fileLock.ReleaseMutex();
+
             return File.ReadLines(file.FullName).Count();
         }
 
-        #endregion Lab8-2
+        #endregion Lab8-3
     }
 }
